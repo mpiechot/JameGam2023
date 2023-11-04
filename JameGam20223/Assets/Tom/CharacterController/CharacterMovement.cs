fac8c6e95cd64b20f052e2172978dfa0fd92a1a5
Tom <15022636+Thaesch@users.noreply.github.com>
2023-11-04 03:23:13 +0100

    Character Movement

--- JameGam20223/Assets/Tom/CharacterController/CharacterMovement.cs
+++ JameGam20223/Assets/Tom/CharacterController/CharacterMovement.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
-
     [SerializeField] private Transform cameraReference;
+
+    [SerializeField] private LayerMask groundLayer;
+
+    [SerializeField] private float groundSlopeLimit;
 
     private Rigidbody characterPhysics;
 
@@ -17,6 +20,18 @@ public class CharacterMovement : MonoBehaviour
 
     public void MoveXZ(float x, float z)
     {
-        characterPhysics.velocity = cameraReference.rotation *  new Vector3(x, 0, z) * moveSpeed;
+        Vector3 nextVelocity = cameraReference.rotation *  new Vector3(x, 0, z) * moveSpeed;
+        // Check for ground Normal and move character only if ground normal is within the slope limit.
+
+        RaycastHit hit;
+        if(Physics.Raycast(characterPhysics.position + nextVelocity * Time.fixedDeltaTime + Vector3.up, Vector3.down, out hit, 1.3f, groundLayer ))
+        {
+            if(Vector3.Dot(hit.normal, Vector3.up) > groundSlopeLimit)
+                characterPhysics.velocity = nextVelocity + Vector3.up * characterPhysics.velocity.y;
+            else
+                characterPhysics.velocity = Vector3.up * characterPhysics.velocity.y;
+        }
+        else
+            characterPhysics.velocity = nextVelocity + Vector3.up * characterPhysics.velocity.y;
     }
 }
